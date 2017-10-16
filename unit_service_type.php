2aed7bed84fefd044f348c84929ef7e98ec34f28
MSIGRENADY <cs.ammarn@gmail.com>
2017-10-16 00:48:44 -0800

    git commit -m

---
+++ unit_service_type.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: GRENADY
+ * Date: 16/10/17
+ * Time: 12:45 ص
+ */
+
+class unit_service_type
+{
+
+
+    protected $unit_id;// auto increament
+    protected  $name;
+
+
+
+}
