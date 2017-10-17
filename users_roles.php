f723b28e69d41f60edb22a7a16c7a3471441b4ae
MSIGRENADY <cs.ammarn@gmail.com>
2017-10-17 08:09:42 -0800

    git commit 'start working in class user_roles'

--- users_roles.php
+++ users_roles.php
@@ -8,5 +8,58 @@
 
 class users_roles
 {
+//role_id	role_number	role_name
 
+protected $id;
+protected  $role_number;
+protected $role_name ;
+
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $role_name
+     */
+    public function setRoleName($role_name)
+    {
+        $this->role_name = $role_name;
+    }
+
+    /**
+     * @param mixed $role_number
+     */
+    public function setRoleNumber($role_number)
+    {
+        $this->role_number = $role_number;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRoleName()
+    {
+        return $this->role_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRoleNumber()
+    {
+        return $this->role_number;
+    
 }
