d04ae7f8cd83b20842d175436531fe7c3ff1aaf7
MSIGRENADY <cs.ammarn@gmail.com>
2017-10-17 08:07:26 -0800

    git commit -m 'done with unit_status class'

--- test.php
+++ test.php
@@ -10,6 +10,7 @@ require_once 'colleges_departments.php';
 require_once 'colleges.php';
 require_once 'unit_file_types.php';
 require_once 'unit_service_type.php';
+require_once 'unit_status.php';
 
 class test
 {
@@ -18,12 +19,13 @@ class test
      public function test_update_colleges_department(){
          error_reporting(E_ALL);
          ini_set('display_errors', 1);
-     $te_object= new  unit_service_type('test','test');
-         $te_object->setId('6');
+     $te_object= new  unit_status('ss','sss','asdasd a new request the status will change to open');
+         $te_object->setId('1');
         // $te_object->setCollegeId('1');
 
 
-         $te_object->update();
+         $te_object->delete_unit_status_object();
+
                   }
 
 
--- unit_status.php
+++ unit_status.php
@@ -13,32 +13,23 @@ class unit_status
     //status_id	status_name	status_code	desc
 
 
-protected $status_id;// autoincreament
+protected $id;// autoincreament
 protected $status_name;
 protected  $status_code;
-protected  $desc;
+protected  $description;
     function __construct($status_name,$status_code,$desc) {
         $this->status_name = $status_name;
-        $this->status_code;
-        $this->desc = $desc;
+        $this->status_code=$status_code;
+        $this->description=$desc;
 
     }
 
     /**
      * @param mixed $desc
      */
-    public function setDesc($desc)
-    {
-        $this->desc = $desc;
-    }
 
-    /**
-     * @param mixed $status_code
-     */
-    public function setStatusCode($status_code)
-    {
-        $this->status_code = $status_code;
-    }
+
+
 
     /**
      * @param mixed $status_id
@@ -56,18 +47,9 @@ protected  $desc;
     /**
      * @return mixed
      */
-    public function getStatusCode()
-    {
-        return $this->status_code;
-    }
 
-    /**
-     * @return mixed
-     */
-    public function getStatusId()
-    {
-        return $this->status_id;
-    }
+
+
 
     /**
      * @return mixed
@@ -78,12 +60,143 @@ protected  $desc;
     }
 
     /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
      * @return mixed
      */
-    public function getDesc()
+    public function getId()
     {
-        return $this->desc;
+        return $this->id;
     }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $status_code
+     */
+    public function setStatusCode($status_code)
+    {
+        $this->status_code = $status_code;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatusCode()
+    {
+        return $this->status_code;
+    }
+
+
+
+
+
+
+    public  function get_unit_status_object()
+    {
+
+        // to check specfic depratment for any clolleges
+        try {
+            $row = R::getAll("SELECT * FROM unitstatus where id='".$this->getId()."'");
+            R::convertToBeans( 'user', $row );
+            if(count($row)>0){
+
+                return $row;
+            }else {
+
+                return null;
+            }
+        }catch (SQLiteException $sq){
+            $sq->getMessage();
+        }
+    }
+
+    public function delete_unit_status_object() {
+
+        try {
+            if(  R::exec( 'delete from  unitstatus    WHERE id ="'.$this->getId().'" ')==1){
+
+            }else{
+                echo"there is issue with delete";
+            }
+        }catch (SQLiteException $sq){
+            $sq->getMessage();
+        }
+    }
+
+
+    function update(){
+
+        try {
+            $status= R::exec( 'update unitstatus set status_name="'.$this->getStatusName().'" ,description= "'.$this->getDescription().'"  , status_code="'.$this->getStatusCode().'"   where id="'.$this->getId().'" ' );
+            if($status==0){
+                echo "can't update the record ";
+            }
+            R::close();
+        }catch (SQLiteException $sq){
+            $sq->getMessage();
+        }
+
+
+    }
+
+
+    function insert() {
+        $unitstatus= R::dispense('unitstatus');
+
+
+
+        $unitstatus->status_name=$this->getStatusName();
+        $unitstatus->status_code=$this->getStatusCode();
+        $unitstatus->description=$this->getDescription();
+
+        $id_redbean_unit_status_id=  R::store($unitstatus); // store is done
+        if($id_redbean_unit_status_id>0){
+
+        }else {
+            echo "the data cannot be inserted in the table unit status table ...";
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
